f3358f2e260b412316af138376d83b7d0d5420d9
Ross Ritter <rossritter@Rosss-MBP.attlocal.net>
2023-11-07 21:09:11 -0500

    had to use the class repo to fix my errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,43 @@
-import { useState } from 'react'
-import ContactList from './assets/ContactList'
+// import { useState } from 'react'
+// import ContactList from './assets/ContactList'
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
 
-import './App.css'
 
-function App() {
-  // const [count, setCount] = useState(0)
-  // const [contacts, setContacts] 
+
+
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
-  )
+  );
 }
 
-export default App
+
+// import './App.css'
+
+// function App() {
+//   // const [count, setCount] = useState(0)
+//   // const [contacts, setContacts] 
+
+//   return (
+//     <>
+//       <ContactList />
+//     </>
+//   )
+// }
+
+// export default App
